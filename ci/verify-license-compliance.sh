1022f4da6e26e746ce0b3c6a640f9122e5bad130
Sean Robertson <seanrobertson@improbable.io>
2019-06-12 14:59:29 +0100

    Dockerize the script

--- ci/verify-license-compliance.sh
+++ ci/verify-license-compliance.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -eET -o pipefail
+[[ -n "${DEBUG:-}" ]] && set -x
 
 SCAN_COMMENT="${BUILDKITE_BUILD_URL:-UnrealGDK Manual Scan}"
 SCAN_TARGET="$(git rev-parse --show-toplevel)"
@@ -37,6 +39,39 @@ log_group_end() {
   _log_group "${1}" "${2}" "end"
 }
 
+run_in_docker() {
+exec docker run \
+  "${interactive_args[@]}" \
+  -e="BUILD_MESSAGE=${BUILD_MESSAGE:-$BUILDKITE_MESSAGE}" \
+  -e="BUILD_NUMBER=${BUILD_NUMBER:-$BUILDKITE_JOB_ID}" \
+  -e="BUILD_URL=${BUILD_URL:-$BUILDKITE_BUILD_URL}" \
+  -e="BUILDKITE_AGENT_ACCESS_TOKEN=${BUILDKITE_AGENT_ACCESS_TOKEN}" \
+  -e="BUILDKITE_AGENT_ENDPOINT=${BUILDKITE_AGENT_ENDPOINT}" \
+  -e="BUILDKITE_AGENT_META_DATA_ENVIRONMENT=${BUILDKITE_AGENT_META_DATA_ENVIRONMENT}" \
+  -e="BUILDKITE_AGENT_META_DATA_QUEUE=${BUILDKITE_AGENT_META_DATA_QUEUE}" \
+  -e="BUILDKITE_ARTIFACT_UPLOAD_DESTINATION=${BUILDKITE_ARTIFACT_UPLOAD_DESTINATION}" \
+  -e="BUILDKITE_BRANCH=${BUILDKITE_BRANCH}" \
+  -e="BUILDKITE_BUILD_CREATOR_EMAIL=${BUILDKITE_BUILD_CREATOR_EMAIL}" \
+  -e="BUILDKITE_BUILD_CREATOR=${BUILDKITE_BUILD_CREATOR}" \
+  -e="BUILDKITE_BUILD_ID=${BUILDKITE_BUILD_ID}" \
+  -e="BUILDKITE_COMMIT=${BUILDKITE_COMMIT}" \
+  -e="BUILDKITE_JOB_ID=${BUILDKITE_JOB_ID}" \
+  -e="BUILDKITE_LABEL=${BUILDKITE_LABEL}" \
+  -e="BUILDKITE_LOCAL_META=${BUILDKITE_LOCAL_META}" \
+  -e="BUILDKITE_MESSAGE=${BUILDKITE_MESSAGE}" \
+  -e="BUILDKITE_ORGANIZATION_SLUG=${BUILDKITE_ORGANIZATION_SLUG}" \
+  -e="BUILDKITE_PARALLEL_JOB_COUNT=${BUILDKITE_PARALLEL_JOB_COUNT}" \
+  -e="BUILDKITE_PARALLEL_JOB=${BUILDKITE_PARALLEL_JOB}" \
+  -e="BUILDKITE_PIPELINE_SLUG=${BUILDKITE_PIPELINE_SLUG}" \
+  -e="BUILDKITE_SOURCE=${BUILDKITE_SOURCE}" \
+  -e="BUILDKITE=${BUILDKITE:-false}" \
+  -e="LOCAL_USER_ID=$(id -u)" \
+  --tmpfs /tmp:exec,mode=777,size=8G \
+  --volume="${repo_base}:/code" \
+  -w /code \
+  "$1:latest" "${@:2}"
+}
+
 # Run the script used to fetch the UnrealGDK deps so we can include them in the scan.
 log_group_start "fetch-unrealgdk-dependencies" "command"
 "${UNREALGDK_SETUP_SCRIPT}"
@@ -54,10 +89,19 @@ log_group_start "whitesource-retrieve-api-key" "command"
 WSS_API_KEY="$(imp-ci secrets read --secret-type="${WSS_SECRET_TYPE}" --secret-name="${WSS_SECRET_NAME}" --environment="${WSS_SECRET_ENV}" --buildkite-org="${WSS_SECRET_BK_ORG}" --field=secretToken)"
 log_group_end "whitesource-retrieve-api-key" "command"
 
+# Build the Docker container we need to run this inside of
+exec docker build \
+  -t "wss-license-scan" \
+  --force-rm \
+  --cache-from "wss-license-scan:latest" \
+  "." \
+  && docker tag "wss-license-scan:" "wss-license-scan:latest"
+
+
 # Run the actual scan and report results to the Improbable Whitesouce console.
 log_group_start "whitesource-scan" "command"
 echo "Running WhiteSource Unified Agent. Expect no output because it is evil."
-java -jar "${WSS_JAR_FILE}" \
+run_in_docker "wss-license-scan" java -jar "${WSS_JAR_FILE}" \
   -apiKey "${WSS_API_KEY}" \
   -wss.url "${WSS_URL}" \
   -project "${WSS_PROJECT_NAME}" \
