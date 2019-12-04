3e61fb2d0b1ce17b16be75cddce13eecb8d2c0f3
Miron Zelina <mironzelina@improbable.io>
2019-12-04 14:37:37 +0000

    Add SpatialFunctionalTest and MapSettings

---
+++ SpatialGDK/Source/SpatialGDKTests/Public/SpatialFunctionalTest.h
@@ -0,0 +1,15 @@
+// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FunctionalTest.h"
+#include "SpatialFunctionalTest.generated.h"
+
+UCLASS(Blueprintable)
+class SPATIALGDKTESTS_API ASpatialFunctionalTest : public AFunctionalTest
+{
+	GENERATED_BODY()
+
+
+};
--- SpatialGDK/Source/SpatialGDKTests/SpatialGDKTests.Build.cs
+++ SpatialGDK/Source/SpatialGDKTests/SpatialGDKTests.Build.cs
@@ -20,7 +20,8 @@ public class SpatialGDKTests : ModuleRules
 				"EngineSettings",
 				"UnrealEd",
 				"Json",
-				"JsonUtilities"
+				"JsonUtilities",
+				"FunctionalTesting"
 			});
 	}
 }
